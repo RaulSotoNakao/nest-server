b7a83460358c70c9f4f3de9f592b3513755d14f1
raulito <raul_0.4_@hotmail.com>
2022-04-25 12:08:57 +0200

    add validation pipe & class validatos in usuario dto

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUsuarioDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateUsuarioDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  nombre: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  password: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  contacto: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  direccion: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  email: string;
+}
--- src/usuario/dto/login-usuario.dto.ts
+++ src/usuario/dto/login-usuario.dto.ts
@@ -1,8 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class LoginUsuarioDto {
   @ApiProperty()
+  @IsNotEmpty()
   nombre: string;
   @ApiProperty()
+  @IsNotEmpty()
   password: string;
 }
